5b141c0ff8edc048f1a2c378685f995696b121b1
libichong <libichong@hotmail.com>
2018-02-09 18:55:31 +0800

    local change

--- bin/init.ps1
+++ bin/init.ps1
@@ -47,12 +47,11 @@ function os([String]$concept)
     ie $newurl;
 }
 
-function aa
+function a([String]$url)
 {
-    for($i=0;$i -lt 16;$i=$i+1)
-    {
-        scope copy D:\Work\DLIS\fastText\vs\FTNative\RecoTopicModel\bin\x64\Debug\Input\Session_UTB_$($i)_segment.csv https://cosmos11.osdinfra.net/cosmos/MMRepository.prod/local/Prod/Video/RecoVideo/TopicModelOutput/Session_UTB_$($i)_segment.csv
-    }
+   D:\Work\RecoVideoObjectStoreCleanup\bin\x64\CH\recoclean.exe D:\tmp\RecoVideo_cleanup_2018_02_05_0.txt
+   D:\Work\RecoVideoObjectStoreCleanup\bin\x64\CH\recoclean.exe D:\tmp\RecoVideo_cleanup_2018_02_05_1.txt
+   D:\Work\RecoVideoObjectStoreCleanup\bin\x64\CH\recoclean.exe D:\tmp\RecoVideo_cleanup_2018_02_05_2.txt
 }
 
 function th([String]$id)
@@ -902,7 +901,9 @@ function put([String]$file)
 {
     $filename = $file.Substring($file.LastIndexOf('\') + 1);
     $localfile = [IO.Path]::GetFullPath($file);
+	Write-Host " Local file:  $localfile"
     $newfile = "https://cosmos11.osdinfra.net/cosmos/MMRepository.prod/my/" + $filename;
+	Write-Host "Remote file:  $newfile"
     pushd "D:\app\ScopeSDK"
     .\Scope.exe copy $localfile $newfile
     popd
@@ -943,7 +944,11 @@ function mm([String]$path)
         {
             $newurl = "https://cosmos11.osdinfra.net/cosmos/MMRepository.prod/my/";
         }
-        elseif($path -eq "12")
+        elseif($path -eq "08")
+        {
+            $newurl = "https://cosmos08.osdinfra.net/cosmos/MMRepository.prod/local/Prod/Video/fromCosmos11/IG/";
+        }
+		elseif($path -eq "12")
         {
             $newurl = "https://cosmos12.osdinfra.net/cosmos/indexGen.Batch.Prod/local/GeoRep/To_cosmos08_MMRepository.prod/Prod/Video/";
         }
@@ -991,6 +996,10 @@ function mm([String]$path)
         {
             $newurl = "https://cosmos11.osdinfra.net/cosmos/MMRepository.prod/local/Prod/Video/RecoVideo/";
         }
+		elseif($path -eq "tsp")
+		{
+			$newurl = "https://cosmos11.osdinfra.net/cosmos/MMRepository.prod/local/Prod/Image/TSP/Partners/VideoIG/Output/";
+		}
 		elseif($path -eq "queue")
 		{
 			$newurl = "https://cosmos11.osdinfra.net/cosmos/MMRepository.prod/_Jobs/?HierarchyNode=KitAllUsers";
@@ -1188,7 +1197,7 @@ function c2img($filename)
     [System.IO.File]::WriteAllBytes($filename+".jpg", $buffer);
 }
 
-function a([string]$url)
+function aa([string]$url)
 {
     grep.exe -r -nHE $url *
 }
@@ -1680,7 +1689,21 @@ function dd
             return;
         }
 
-        $result = & (Resolve-Path $ES).Path $args
+        $tabs = $search.Split(' \ /', [System.StringSplitOptions]::RemoveEmptyEntries)
+        $result = & (Resolve-Path $ES).Path $tabs[$tabs.Length-1]
+        for($i = 0; $i -lt $tabs.Length - 1;$i++)
+        {
+            $newResult =  [System.Collections.ArrayList]@()
+            for($j=0;$j -lt $result.Count;$j++)
+            {
+                if($result[$j].ToLower().Contains($tabs[$i].ToLower()))
+                {
+                    $newResult.Add($result[$j]) | Out-Null
+                }
+            }
+            $result = $newResult;
+        }
+
         if($result.Count -lt 1){
             Write-Host -ForegroundColor Red "Not found the directory: $search";
             return;
@@ -2277,3 +2300,63 @@ function Get-FileHex
     $paddedhex
     }
 }
+
+Set-Alias g git
+#==================================================================================================
+# Functions
+#==================================================================================================
+
+function Git-AddAll              { git add -A }
+function Git-Branch              { git branch -v }
+function Git-Checkout            { git checkout $args }
+function Git-CheckoutPartial     { git checkout -p -- $args }
+function Git-CommitAll           { git add -A
+                                   git commit -m $args }
+function Git-Diff                { git diff }
+function Git-DiffStaged          { git diff --staged }
+function Git-Delete              { git branch -d $args }
+function Git-DiffTool            { git difftool }
+function Git-Fetch               { git fetch }
+function Get-FetchPrune          { git fetch --prune }
+function Git-GetStatus           { git status }
+function Git-Log                 { git log --decorate --graph --oneline }
+function Git-LogAll              { git log --all --decorate --graph --oneline }
+function Git-NewBranch           { git checkout -b $args }
+function Git-Pull                { git pull }
+function Git-PushOriginHead      { git push origin HEAD }
+function Git-PushWithForce       { git push -f origin HEAD }
+function Git-PushWithUpstream    { git push -u origin HEAD }
+function Git-RebaseAbort         { git rebase --abort }
+function Git-RebaseContinue      { git rebase --continue } 
+function Git-RebaseInteractive   { git rebase -i master }
+function Git-ResetHard           { git reset --hard }
+function Git-Show                { git show $args }
+
+#==================================================================================================
+# Aliases
+#==================================================================================================
+
+Set-Alias gaa Git-AddAll
+Set-Alias gb Git-Branch
+Set-Alias gbr Git-NewBranch
+Set-Alias gca Git-CommitAll
+Set-Alias gco Git-Checkout
+Set-Alias gcp Git-CheckoutPartial
+Set-Alias gd  Git-Delete
+Set-Alias gdf Git-Diff
+Set-Alias gds Git-DiffStaged
+Set-Alias gdt Git-DiffTool
+Set-Alias gf Git-Fetch
+Set-Alias gfp Get-FetchPrune
+Set-Alias glg Git-Log
+Set-Alias glo Git-LogAll
+Set-Alias gri Git-RebaseInteractive
+Set-Alias gpu Git-Pull
+Set-Alias gra Git-RebaseAbort
+Set-Alias grc Git-RebaseContinue
+Set-Alias grh Git-ResetHard
+Set-Alias gs Git-GetStatus
+Set-Alias gsh Git-Show
+Set-Alias push Git-PushOriginHead
+Set-Alias pushf Git-PushWithForce
+Set-Alias pushu Git-PushWithUpstream
